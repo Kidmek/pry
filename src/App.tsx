038a74c5fc12ee000dbd15bde5969cfc16d2ab3e
Kidus Mekonnen <kidusmekonnen591@gmail.com>
2023-10-25 10:30:37 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <input />
+    </div>
+  )
+}
+
+export default App
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const URL: string =
+  'https://652f91320b8d8ddac0b2b62b.mockapi.io/autocomplete'
